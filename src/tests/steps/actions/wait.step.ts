5461a4b3898ba4d9b9341c346941e8738c56ecca
jamil2018 <hasnatjamil2018@gmail.com>
2026-01-24 20:57:34 +0600

    add navigation, wait and basic visibility template steps

--- src/tests/steps/actions/wait.step.ts
+++ src/tests/steps/actions/wait.step.ts
@@ -1,23 +1,52 @@
 /**
  * @name wait
- * @description Template steps that handles waiting actions
+ * @description Template steps that handles waiting
  * @type ACTION
  */
 import { When } from '@cucumber/cucumber'
 import { CustomWorld } from '../../config/executor/world.js'
+import { SelectorName } from '@/types/locator/locator.type'
 
 // This file is generated automatically. Add template steps to this group to generate content.
 
 /**
- * @name Wait
- * @description Template step that handles waiting for specific amount of time
+ * @name wait for page load
+ * @description Template step for waiting till page becomes interactive
  * @icon WAIT
  */
-When('the user waits for {int} seconds', async function (this: CustomWorld, waitTime: number) {
+When('the user waits for the current page to be loaded', async function (this: CustomWorld) {
   try {
-    await this.page.waitForTimeout(waitTime * 1000)
+    await this.page.waitForLoadState('domcontentloaded')
   } catch (error) {
-    console.error(error)
-    throw error
+    throw new Error(`Failed to wait for the current page to be loaded: ${error}`)
   }
 })
+
+/**
+ * @name wait for url route
+ * @description Template step for waiting for a url route to be loaded
+ * @icon WAIT
+ */
+When('the user waits for the route {string} to be loaded', async function (this: CustomWorld, routeName: string) {
+  try {
+    await this.page.waitForURL(routeName, { waitUntil: 'domcontentloaded' })
+  } catch (error) {
+    throw new Error(`Failed to wait for the route ${routeName} to be loaded: ${error}`)
+  }
+})
+
+/**
+ * @name wait for element
+ * @description Template step for waiting for element to become visible
+ * @icon WAIT
+ */
+When(
+  'the user waits for the element {string} to become visible',
+  async function (this: CustomWorld, elementName: SelectorName) {
+    try {
+      await this.page.waitForSelector(elementName, { state: 'visible' })
+    } catch (error) {
+      throw new Error(`Failed to wait for the element ${elementName} to become visible: ${error}`)
+    }
+  },
+)
---
+++ src/tests/steps/validations/visibility.step.ts
@@ -0,0 +1,26 @@
+/**
+ * @name visibility
+ * @description Template step for validating element visibility
+ * @type VALIDATION
+ */
+import { When } from '@cucumber/cucumber'
+import { CustomWorld, expect } from '../../config/executor/world.js'
+import { SelectorName } from '@/types/locator/locator.type'
+import { resolveLocator } from '../../utils/locator.util.js'
+
+// This file is generated automatically. Add template steps to this group to generate content.
+
+/**
+ * @name validate element visibility
+ * @description Template step for validating the visibility of an element
+ * @icon VALIDATION
+ */
+When('the element {string} should be visible', async function (this: CustomWorld, elementName: SelectorName) {
+  try {
+    const element = this.page.locator(elementName)
+    const isVisible = await element.isVisible({ timeout: 10000 })
+    expect(isVisible).to.be.true
+  } catch (error) {
+    throw new Error(`Failed to validate the visibility of the element ${elementName}: ${error}`)
+  }
+})
