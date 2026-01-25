ad8b5f046e24d619191dcd6a218997c82db6c41c
Hasnat Jamil <hasnatjamil2018@gmail.com>
2026-01-25 20:01:13 +0600

    start implementation of click steps

---
+++ src/tests/steps/actions/click.step.ts
@@ -0,0 +1,31 @@
+/**
+ * @name click
+ * @description Template step for handling element click
+ * @type ACTION
+ */
+import { When } from '@cucumber/cucumber';
+import { CustomWorld } from '../../config/executor/world.js';
+import { SelectorName } from '@/types/locator/locator.type';
+import { resolveLocator } from '../../utils/locator.util.js';
+
+// This file is generated automatically. Add template steps to this group to generate content.
+
+/**
+ * @name click
+ * @description Template step for clicking on an element
+ * @icon MOUSE
+ */
+When(
+  'the user clicks on the {string} element',
+  async function (this: CustomWorld, elementName: SelectorName) {
+    const selector = await resolveLocator(this.page, elementName)
+    if (!selector) {
+      throw new Error(`Selector ${elementName} not found`)
+    }
+    try {
+      await this.page.locator(selector).click()
+    } catch (error) {
+      throw new Error(`Failed to click on the ${elementName} element: ${error}`)
+    }
+  },
+)
